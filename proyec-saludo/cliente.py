2da550670bde7e591c52e5bfcc7cb7f5384e3d11
KeniaCardenas01 <kenianairobicardenasnegron@gmail.com>
2025-03-14 20:49:42 -0400

    Add files via upload

--- proyec-saludo/cliente.py
+++ proyec-saludo/cliente.py
@@ -24,5 +24,12 @@ class cliente:
     
         #se crea metodos normales de la clase
     
-    def tomar_datos(self):
+        def tomar_datos(self):
+            self.nombre_cliente = input("digite el nombre del cliente:  ")
+            self.apellido_cliente = input("digite el apelllido del cliente")
         
+        def procesar_datos(self):
+            aux= self.nombre_cliente +" "+ self.apellido_cliente
+            
+        def mostrar_info_cliente(self):
+            print(f"nombre del cliente:){self.nombre_cliente}-apellido del cliente:{self.apellido_cliente}")    
